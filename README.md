945301872c9cd16e1dbe54c20e97d5f525eda8b5
stringer <stringer@str.local>
2018-12-19 15:46:01 +0100

    Initial fixes

--- README.md
+++ README.md
@@ -5,18 +5,29 @@ This Repository holds the Database, Backend and Frontend for an `ExamLendOut`-Se
 The individual modules are executed as docker containers. 
 
 
-##Status
+## Status
+`
 Currently the System is not ready for deployment. 
-Is is in work right now!
+It is in work right now
+`
+- 
 
 
+## Configuration
+You have to create your own .env files in the directories `/backend`, `/frontend` & `database` and configure them according to the already existing .env.example templates.
+> Here you can set ports & passwords 
 
 ## Start 
 1. Install Docker on your Machine
-2. Run the `Docker-compose-staging` file with command:
+2. Run the `Docker-compose-staging.yml` file with command:
 > docker-compose up
-3. Access frontend via `localhost:4200`
+3. Access frontend via `localhost:port`
+
 
 
 ## Develope
-Here for you have to build the docker container with the `docker-compose` file without `staging`
+Here for you have to build the docker container with the `docker-compose.yml` file.
+1. You must allow Docker to access this directory on your host machine. You can set this option the `File Sharing`-Preference. 
+2. Run the `docker-compose.yml` file. 
+
+3. Now you are able to access the backend directly on the port set in your .env file in the `/backend` folder 
---
+++ database/init.sql
@@ -0,0 +1,56 @@
+CREATE TABLE summary(
+    idy int NOT NULL AUTO_INCREMENT,
+    data VARCHAR(255) NOT NULL, 
+    id_user INT NOT NULL, 
+    id_exam INT NOT NULL,  
+    created TIMESTAMP NOT NULL, 
+    updated TIMESTAMP NOT NULL,
+    PRIMARY KEY(idy), 
+    FOREIGN KEY (id_user) REFERENCES user(idr), 
+    FOREIGN KEY (id_exam) REFERENCES exam(idm)
+    );
+
+CREATE TABLE solution(
+    idn int NOT NULL AUTO_INCREMENT,
+    data VARCHAR(255) NOT NULL,
+    id_user INT NOT NULL, 
+    id_exam INT NOT NULL,  
+    created TIMESTAMP NOT NULL, 
+    updated TIMESTAMP NOT NULL,
+    PRIMARY KEY(idn), 
+    FOREIGN KEY (id_user) REFERENCES user(idr), 
+    FOREIGN KEY (id_exam) REFERENCES exam(idm)
+    );
+
+CREATE TABLE exam(
+    idm int NOT NULL AUTO_INCREMENT,
+    data VARCHAR(255) NOT NULL,
+    created TIMESTAMP NOT NULL, 
+    updated TIMESTAMP NOT NULL,
+    semester varchar(24) NOT NULL,
+    id_modul INT NOT NULL, 
+    prof VARCHAR(50) NOT NULL,
+    aid INT,
+    notes varchar(500),  
+    PRIMARY KEY(idm), 
+    FOREIGN KEY (id_modul) REFERENCES modul(idl)
+    );
+
+CREATE TABLE modul(
+    idl int NOT NULL AUTO_INCREMENT,
+    semster INT NOT NULL, 
+    cp INT NOT NULL, 
+    id_user INT,
+    PRIMARY KEY(idl), 
+    FOREIGN KEY (id_user) REFERENCES user(idr)
+    );
+
+CREATE TABLE user(
+    idr int NOT NULL AUTO_INCREMENT,
+    name varchar(30) NOT NULL, 
+    password varchar(128) NOT NULL, 
+    permissionlevel INT NOT NULL,
+    id_modul INT, 
+    PRIMARY KEY(ID), 
+    FOREIGN KEY (id_modul) REFERENCES modul(idr)
+    );
